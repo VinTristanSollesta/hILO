9b746849e8f4cefed02af1cb021b5af0e5871d22
VinTristanSollesta <vintristan.sollesta@gmail.com>
2024-10-19 21:19:42 +0800

    +added colorExtract screen

---
+++ screens/ColorExtract.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+
+const ColorExtract = () => {
+  const [colors, setColors] = useState([]);
+
+  useEffect(() => {}, []);
+  return (
+    <View>
+      <View>Test 1234</View>
+    </View>
+  );
+};
+
+export default ColorExtract;
